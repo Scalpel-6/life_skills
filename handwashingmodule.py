f81a7ed3d440bc3894f89ba0fed2fd5c78295a0d
Daniel Liedl <dan.personal006@gmail.com>
2024-07-02 19:35:06 -0700

    Initial commit with .gitignore and .py files

---
+++ handwashingmodule.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import time
+
+def main():
+    while True:
+
+        restart = input("\nDo you want to restart?  Enter yes or no.\n")
+        if restart.lower() != 'yes':
+            break
+
+if __name__="__main__":
+    main()
